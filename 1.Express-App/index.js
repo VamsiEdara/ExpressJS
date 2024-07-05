039ddd39e6fe2faf9749a4ae764123dfcc906ffb
Vamsi Edara 26 <96575839+VamsiEdara@users.noreply.github.com>
2024-07-05 21:45:00 +0530

    Update index.js

--- 1.Express-App/index.js
+++ 1.Express-App/index.js
@@ -3,7 +3,7 @@
 // 3. Get Express Into Your Project (require("express"))
 // 4. Create instance of Express (const app = express())
 // 5. Provide port (app.listen(5000, () => console.log("port 5000")))
-// 6. basic rout (app.get(path, callback))
+// 6. basic rout (app.get(path, callback function)) --> This callback has two arguments 1) req 2) res
 // 7. Install nodemon if you require
 
 import express from "express";
